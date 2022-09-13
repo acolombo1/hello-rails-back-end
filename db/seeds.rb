dcb7fed93c1d1cd5008da0e71066682fa520cb14
Aldo Aníbal Colombo <44879977+acolombo1@users.noreply.github.com>
2022-09-13 19:48:25 -0300

    finish api server

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Greeting.create(greeting: "Hi World")
+Greeting.create(greeting: "Hello World")
+Greeting.create(greeting: "Ciao Mondo")
+Greeting.create(greeting: "Merry Christmas")
+Greeting.create(greeting: "Hola Mundo!!!")
